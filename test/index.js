67ac936faed2db2e792c6af48985b182a7146070
Jeff Escalante <hello@jenius.me>
2016-06-30 14:44:34 -0400

    working on the parser

--- test/index.js
+++ test/index.js
@@ -1,30 +1,36 @@
-const parse = require('..')
+const parser = require('..')
 const fs = require('fs')
+const util = require('util')
 const path = require('path')
 const test = require('ava')
 const fixtures = path.join(__dirname, 'fixtures')
 
-test('lexer', (t) => {
+test('parser', (t) => {
+  const html = fs.readFileSync(path.join(fixtures, 'nesting.html'), 'utf8')
+  console.log(util.inspect(parser(html), { showHidden: false, depth: null, colors: true }))
+})
+
+test.skip('lexer', (t) => {
   const html = fs.readFileSync(path.join(fixtures, 'simple.html'), 'utf8')
-  console.log(parse(html))
+  console.log(parser.lex(html))
 })
 
 test.skip('pipe', (t) => {
   const html = fs.readFileSync(path.join(fixtures, 'pipe.html'), 'utf8')
-  console.log(parse(html))
+  console.log(parser(html))
 })
 
 test.skip('id', (t) => {
   const html = fs.readFileSync(path.join(fixtures, 'id.html'), 'utf8')
-  console.log(parse(html))
+  console.log(parser(html))
 })
 
 test.skip('class', (t) => {
   const html = fs.readFileSync(path.join(fixtures, 'class.html'), 'utf8')
-  console.log(parse(html))
+  console.log(parser(html))
 })
 
 test.skip('comment', (t) => {
   const html = fs.readFileSync(path.join(fixtures, 'comments.html'), 'utf8')
-  console.log(parse(html))
+  console.log(parser(html))
 })
